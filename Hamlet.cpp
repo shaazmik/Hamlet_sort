15fb0f75311d7e6b1b38ad82a512cdfc96e856b3
Daniil <45428240+shaazmik@users.noreply.github.com>
2021-08-29 23:58:41 +0300

    Add files via upload

--- Hamlet.cpp
+++ Hamlet.cpp
@@ -1,114 +1,90 @@
 #include "Hamlet.h"
 
-//{
-size_t count_line_in_text(FILE *in)
-{
-    if (!in) return -1;
-
-    char not_used[100] = "";
-
-    int quantity_lines = 0;
-
-    while (fgets(not_used, 100, in) != NULL)
-        quantity_lines++;
-
-    return quantity_lines;
-}
-
-
-size_t find_file_size(FILE *in)
-{
-
-    assert (in != NULL);
-
-    int size_of_file = 0;
-
-    fseek (in, 0, SEEK_END);
-
-    size_of_file = ftell (in);
-
-    fseek (in, 0, SEEK_SET);
-
-    return (size_of_file);
-}
-//}
-
-//{
-char* fill_text(FILE* in, size_t file_size)
-{
-    assert(in != NULL);
-
-    char* text_array = (char*) calloc(file_size, sizeof(char));
-
-    fread(text_array, sizeof(char), file_size, in);
-
-    return text_array;
-}
-
-struct Line* navigation_in_line(struct Line* str_array, *char text_array, size_t quantity_lines)
-{
-    assert(text_array != NULL);
-    assert(str_array != NULL);
-
-    struct Line* str_array = (struct Line*) calloc(quantity_lines, sizeof(struct Line))
-
-    char* pointer_start = text_array;
-    char* pointer_ne_start = text_array;
-
-    for (int i = 0; i < quantity_lines; i++)
-    {
-        pointer_ne_start = strchr(pointer_start, '\n');
-
-        *pointer_ne_start = '\0';
-
-        str_array[i].start_line = pointer_start;
-        str_array[i].len = pointer_ne_start - pointer_start + 1;
-
-        pointer_start = pointer_ne_start + 2; // +2 because in win10 end of line has \r symbol.
-    }
-    return str_array;
-};
-//}
-
-
-//{
-void zapuskTRTRv2(FILE* in, struct Text* Denmark_sht)
-{
-    assert(in != NULL);
-    assert(Denmark_sht != NULL);
-
-    Denmark_sht->size_of_text = find_file_size(in);
-    Denmark_sht->quantity_lines = count_line_in_text(in);
-    Denmark_sht->text_array = fill_text(in, Denmark_sht->size_of_text);
-    Denmark_sht->linii = navigation_in_line(Denmark_sht->linii, Denmark_sht->text_array, Denmark_sht->quantity_lines);
-
-    return;
-}
-
-
-void VJUXv2(FILE* out, const struct Text* Denmark_sht)
-{
-    assert(in != NULL);
-    assert(Denmark_sht != NULL);
-
-    for (int num_of_line = 0; num_of_line < Denmark_sht->quantity_lines, i++)  //
-    {
-        fputs(Denmark_sht->linii[num_of_line].start_line, out);
-        fputc('\n', out);
-    }
-}
-//}
-
 
 int main()
 {
+    setlocale(LC_ALL, "ru_RU.cp1251");
+
     struct Text Denmark_sht = {};
 
-    FILE *in = fopen ("Hamlet.bin", "r");
+    char name[100] = "";
 
-    zapuskTRTRv2(in, &Denmark_sht);
+    printf("Input name text file:\n");
 
-    //printf ("%d  %d", find_file_size(in), count_line_in_text(in));//171509
+    scanf("%s", name);
+
+    FILE *in = fopen (name, "rb");
+
+    if (check_input_file(in) == -1) return 1;
+
+    if (input_struct(in, &Denmark_sht) == 1)
+    {
+        printf("null pointer of text_array\n");
+        return 1;
+    }
 
     fclose(in);
+
+    paramon_sort(Denmark_sht.linii, Denmark_sht.quantity_lines, comparator_paramonator);
+
+    FILE* out = fopen("Text_sorted.txt", "w");
+    assert(out != nullptr);
+
+    fprintf(out, "Sorted text\n\n");
+    output_file(out, &Denmark_sht);
+    fprintf(out, "\n\n");
+
+    fclose(out);
+
+    bubble_sort(Denmark_sht.linii, Denmark_sht.quantity_lines, comparator_refrigerator);
+
+    out = fopen("Text_sorted.txt", "a");
+    assert(out != nullptr);
+
+    fprintf(out, "Reverse sorted text\n\n");
+    output_file(out, &Denmark_sht);
+    fprintf(out, "\n\n");
+
+    fprintf(out, "Original text\n\n");
+    output_text_array(out, &Denmark_sht);
+    fclose(out);
+
+
+    free_memory(&Denmark_sht);
+
+
+printf ("__________________________________________________111_______\n"  // Не без кота
+        "________________________________________________¶¶¶¶¶_______\n"
+        "______________________________________________¶¶¶¶1_________\n"
+        "_____________________________________________¶¶¶____________\n"
+        "____________________________________________¶¶¶_____________\n"
+        "____________________________________________¶¶¶_____________\n"
+        "_____________________________________________¶¶¶¶1111_______\n"
+        "______________________________________________1¶¶¶¶¶¶¶¶_____\n"
+        "____________________________________________________1¶¶¶____\n"
+        "__________________________1¶¶_________________________¶¶¶___\n"
+        "________________________1¶¶¶1_________________________¶¶¶___\n"
+        "_________11___________1¶¶¶11__________________________¶¶¶___\n"
+        "_________1¶¶¶¶¶11¶¶¶¶¶¶¶¶¶¶¶1____________________11¶¶¶¶¶____\n"
+        "__________1¶¶¶¶¶¶¶¶¶¶¶¶__11¶¶¶_________________1¶¶¶¶¶¶¶¶¶¶__\n"
+        "____________1¶¶1_¶1¶¶¶¶_¶¶¶¶11_______________1¶¶¶¶¶¶¶¶¶¶¶¶¶_\n"
+        "____________1¶1__11¶¶¶¶¶¶¶¶____111_______1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_\n"
+        "____________¶¶¶¶¶¶¶¶1_¶¶¶1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1_\n"
+        "__________________1¶¶1111___1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1__\n"
+        "______________________________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶___\n"
+        "______________________________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1_¶¶¶¶1___\n"
+        "______________________________¶¶¶¶¶¶¶¶¶¶¶¶¶1__¶¶¶1__¶¶¶¶¶1__\n"
+        "________________________111¶¶¶¶¶¶__1¶¶¶¶______1¶¶____1¶¶¶¶¶_\n"
+        "_______________________¶¶¶¶¶1________¶¶¶_______¶¶_______¶¶¶_\n"
+        "______________________¶¶¶_____________¶¶¶_____1¶¶________¶¶_\n"
+        "_____________________¶¶¶¶_____________1¶¶__1¶¶¶¶¶________¶¶_\n"
+        "_____________________¶¶¶¶_____________1¶¶____11________11¶¶1\n"
+        "______________________11_______________¶¶______________¶¶¶¶1\n"
+        "_____________________________________11¶¶___________________\n"
+        "____________________________________1¶¶¶¶___________________\n");
+
+
+return 0;
+//    FILE *out = fopen("Hamlet_sorted.txt", "a");
 }
+
